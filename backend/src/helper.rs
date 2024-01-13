9ceba0f2dec6205d329a5e14a465fca34538138c
Jiaming Li <ljm625@gmail.com>
2024-01-13 15:39:14 +0800

    fix

--- backend/src/helper.rs
+++ backend/src/helper.rs
@@ -60,7 +60,7 @@ pub fn check_yaml(str: &String) -> bool {
 }
 pub fn check_json(str: &String) -> bool {
     if let Ok(x) = serde_json::from_str::<serde_json::Value>(str) {
-        if let Some(value) = x.get("inbound") {
+        if let Some(value) = x.get("inbounds") {
             return true;
         } else {
             return false;
